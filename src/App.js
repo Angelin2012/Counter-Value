de63726882c3e7a01ca8a8983c518dc313817d6c
Angel <angel.inkeziah14@gmail.com>
2025-06-30 15:18:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,37 @@ import Counter from './counter';
 
 function App() {
    const[darkMode, setDarkMode]= useState(false);
+   const [showContent, setShowContent] = useState(false);
+   const [name, setName] = useState("");
+
   return (
     <div className = {darkMode ?  'container dark' : 'container light'}>
+      <button className={showContent ? 'off-btn' : 'on-btn'}
+        onClick={() => setShowContent(!showContent)}>
+        {showContent ? "Dont show text" : "Show Text"}
+        </button>
+
+        {showContent && (
+          <>
        <h1>Click Counter App</h1>
+       <input 
+       type="text"
+       placeholder='Enter your name'
+       value={name}
+       onChange={(e)=>setName(e.target.value)}/>
+       <p>Heloo,{name? name:"User"}</p>
        <Counter/>
-       <button className="btn"onClick={() => setDarkMode(!darkMode)}>
+
+
+       <button className="btn" onClick={() => setDarkMode(!darkMode)}>
         {darkMode ? 'switch to Light Mode' : 'Switch to darkMode'}
        </button>
+       </>
+        )}
    </div>    
   );
 }
 
 export default App;
+
+//Two way binding - changes in the ui(like form inputs) update the components the component state.
